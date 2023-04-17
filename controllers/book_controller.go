cb87482892dec0a08694bf0b562281ee85416b12
Michel Kim <kim98.michel@gmail.com>
2023-04-17 12:24:43 -0300

    create routes and  controllers

--- controllers/book_controller.go
+++ controllers/book_controller.go
@@ -1,9 +1,82 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kimMichel/webapi-mvc/database"
+	"github.com/kimMichel/webapi-mvc/models"
+)
 
 func ShowBook(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"value": "ok",
-	})
+	id := c.Param("id")
+
+	newId, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "ID has to be integer",
+		})
+
+		return
+	}
+
+	db := database.GetDatabase()
+
+	var book models.Book
+	err = db.First(&book, newId).Error
+	if err != nil {
+		if err != nil {
+			c.JSON(400, gin.H{
+				"error": "cannot find book " + err.Error(),
+			})
+
+			return
+		}
+	}
+
+	c.JSON(200, book)
+
+}
+
+func CreateBook(c *gin.Context) {
+	db := database.GetDatabase()
+
+	var book models.Book
+
+	err := c.ShouldBindJSON(&book)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot bind JSON " + err.Error(),
+		})
+
+		return
+	}
+
+	err = db.Create(&book).Error
+
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot create book " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, book)
+}
+
+func ShowBooks(c *gin.Context) {
+	db := database.GetDatabase()
+
+	var books []models.Book
+	err := db.Find(&books).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "cannot list books " + err.Error(),
+		})
+
+		return
+	}
+
+	c.JSON(200, books)
 }
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/kimMichel/webapi-mvc/server"
+import (
+	"github.com/kimMichel/webapi-mvc/database"
+	"github.com/kimMichel/webapi-mvc/server"
+)
 
 func main() {
+	database.StartDB()
+
 	server := server.NewServer()
 
 	server.Run()
